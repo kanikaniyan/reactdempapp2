dc0febe463c30ad692d3cfccbcdc57f6f55863ea
kanikaniyan <kkaniyan96@gmail.com>
2023-10-07 16:03:56 +0530

    comiit

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+// const mongoose = require("mongoose");
+const port = process.env.PORT || 3002;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log("Database is connected..."))
+//   .catch((err) => console.log(err));
+
+//db schema
+// const userSchema = mongoose.Schema({
+//   name: String,
+//   lastName: String,
+// });
+
+//db model
+// const User = new mongoose.model("User", userSchema);
+
+app.get("/get-users", (req, res) => {
+  res.json({name: 'kaniyan', lastName: 'karuthavel pandian'})
+});
+
+app.post("/create", (req, res) => {
+  //save to mongodb and send response
+  // const newUser = new User({
+  //   name: req.body.name,
+  //   lastName: req.body.lastName,
+  // });
+
+  res.json({success: 'created'})
+  
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on post ${port}`);
+});
